f32e83366d7c4e2d5638afaa5a29d3b805fd61f1
Dany <danyramirezg@gmail.com>
2018-09-24 22:22:58 -0700

    Minus

--- src/ramirez/Main.java
+++ src/ramirez/Main.java
@@ -1,8 +1,18 @@
 package ramirez;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
-	    System.out.println("DanySlava are the best :*");
+        int result1 = minus(8, 4);
+        int result2 = minus(10, 5);
+
+        System.out.println("result2 = " + result2);
+        System.out.println("result1 = " + result1);
+
+    }
+
+    static int minus(int num1, int num2) {
+        return num1 - num2;
     }
 }
+
